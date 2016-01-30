c5ea4f0a60a48a9e3261a9b3328e29f8b390c0fc
Jo Balme <jo.balme@gmail.com>
2016-01-30 01:21:23 -0800

    Basic leddar io

--- arduino/chomp/chomp.ino
+++ arduino/chomp/chomp.ino
@@ -1,17 +1,38 @@
+#include <Leddar.h>
+
 // Xbee configuration notes:
 // Followed tutorial here: https://eewiki.net/display/Wireless/XBee+Wireless+Communication+Setup
 // Xbee SN 13A200 40BEFC5C is set to Coordinator AT, and DH/DL programmed to the SN of the Router AT
 // Xbee SN 13A200 40B9D1B1 is set to Router AT, and DH/DL programmed to the SN of the Coordinator AT
 // They're talking on PAN ID 2001 (A Space Odyssey)
-HardwareSerial & Xbee = Serial;
+//HardwareSerial & Xbee = Serial;
+HardwareSerial & TTL = Serial3;
+HardwareSerial & LeddarSerial = Serial;
+
+Leddar16 leddar(115200,1);
 
 void setup() {
-  // put your setup code here, to run once:
-  Xbee.begin(9600);
+  // Xbee.begin(9600);
+  TTL.begin(9600);
+  LeddarSerial.begin(115200);
+  leddar.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Xbee.write("A");
-  //delay(100);
+  // Xbee.write("A");
+  char detections = leddar.getDetections();
+  TTL.write("Detections: ");
+  TTL.print(detections, DEC);
+  TTL.write("\r\n");
+  if (detections >= 0){
+    for (int i = 0; i < leddar.NbDet; i++){
+      TTL.print(leddar.Detections[i].Segment, DEC);
+      TTL.write("/");
+      TTL.print(leddar.Detections[i].Distance, DEC);
+      TTL.write(" ");
+      //leddar.Detections[i].Amplitude;
+    }
+    TTL.write("\r\n");   
+  }
+  delay(500);
 }
