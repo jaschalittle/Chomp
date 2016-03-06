1837a48c7fa15b76a2e0a8b955e465376c05aa39
Jo Balme <jo.balme@gmail.com>
2016-03-05 19:11:40 -0800

    Updating pins file

--- arduino/chomp/pins.h
+++ arduino/chomp/pins.h
@@ -4,9 +4,17 @@
 #define GREEN 21
 #define RED 22
 
+// RC fast interrupt channels (note: these are not pin numbers, they are interrupt numbers!)
+#define WEAPON_ENABLE 5
+
+// RC pin bank channels
+
+
+// Sensors
 #define PRESSURE_AI 7
 #define ANGLE_AI 15
 
+// Valve DOs
 #define ENABLE_VALVE_DO 14
 #define THROW_VALVE_DO 15
 #define VENT_VALVE_DO 28
