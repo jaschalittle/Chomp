c064133c4704675a8136b88959e47ba530a08c6a
aaroncooper <aaroncooper@users.noreply.github.com>
2016-03-08 23:34:35 -0800

    Create chump chomp shared PWM drive code

---
+++ arduino/pwm_drive.h
@@ -0,0 +1,16 @@
+#ifndef PWM_DRIVE_H
+#define PWM_DRIVE_H
+
+const float pwm_neutral = 1500/20000.0;
+
+void pwm_duty_L(float duty);
+
+void pwm_duty_R(float duty);
+
+void pwm_output_setup();
+
+void targeting_disable();
+
+void targeting_enable();
+
+#endif // PWM_DRIVE_H
