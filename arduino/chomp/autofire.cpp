4eb0d9c9076e6a264838e639614dfae9526a9d39
Jo Balme <jo.balme@gmail.com>
2016-03-12 00:31:56 -0800

    Separating which segments are used for arming vs firing

--- arduino/chomp/autofire.cpp
+++ arduino/chomp/autofire.cpp
@@ -2,8 +2,11 @@
 #include "autofire.h"
 #include "leddar_io.h"
 
-#define CENTER_ZONE_MIN 4
-#define CENTER_ZONE_MAX 12
+#define CENTER_ZONE_ARM_MIN 4
+#define CENTER_ZONE_ARM_MAX 12
+
+#define CENTER_ZONE_FIRE_MIN 6
+#define CENTER_ZONE_FIRE_MAX 10
 
 #define FACE_OFFSET 20
 #define ARM_THRESHOLD 150
@@ -14,7 +17,7 @@ LeddarState get_state(unsigned int detection_count, Detection* detections){
   int last_detected_segment = 0;
   int contiguous = 1;
   for (int i = 0; i < detection_count; i++){
-    if (detections[i].Segment < CENTER_ZONE_MAX && detections[i].Segment > CENTER_ZONE_MIN &&
+    if (detections[i].Segment < CENTER_ZONE_FIRE_MAX && detections[i].Segment > CENTER_ZONE_FIRE_MIN &&
         detections[i].Distance - FACE_OFFSET < FIRE_THRESHOLD){
 //      Serial.print(detections[i].Segment);
 //      Serial.write("/");
@@ -36,7 +39,7 @@ LeddarState get_state(unsigned int detection_count, Detection* detections){
   last_detected_segment = 0;
   contiguous = 1;
   for (int i = 0; i < detection_count; i++){
-    if (detections[i].Segment < CENTER_ZONE_MAX && detections[i].Segment > CENTER_ZONE_MIN &&
+    if (detections[i].Segment < CENTER_ZONE_ARM_MAX && detections[i].Segment > CENTER_ZONE_ARM_MIN &&
         detections[i].Distance - FACE_OFFSET < ARM_THRESHOLD){
       //Xbee.write("Hit!\r\n");
       if (detections[i].Segment - last_detected_segment == 1){
