b340f7d72a2ac035db4025fa96f66f28d690be6a
Aaron Cooper <aaroncooper@ucla.edu>
2016-03-22 10:41:23 -0700

    Change serial IO setup

--- arduino/chomp/chomp.ino
+++ arduino/chomp/chomp.ino
@@ -1,15 +1,10 @@
 #include "chump_main.h"
-#include "SoftwareSerial.h"
 
-extern HardwareSerial& Debug = Serial;          // RX pin 0, TX pin 1
-extern HardwareSerial& Xbee = Serial1;          // RX pin 19, TX pin 18
+// extern HardwareSerial& Debug = Serial;
+extern HardwareSerial& Xbee = Serial;           // RX pin 0, TX pin 1
+extern HardwareSerial& DriveSerial = Serial1;   // RX pin 19, TX pin 18
 extern HardwareSerial& LeddarSerial = Serial2;  // RX pin 17, TX pin 16
 extern HardwareSerial& Sbus = Serial3;          // RX pin 15, TX pin 14
-// pins 22 and 23 not pinned out on Grove shield, so they are safe to "throw away" to initialize TX
-SoftwareSerial mySerial4(22, 6);                // RX pin 22, TX pin 6
-SoftwareSerial mySerial5(23, 7);                // RX pin 23, TX pin 7
-extern SoftwareSerial& LeftWheelSerial = mySerial4;
-extern SoftwareSerial& RightWheelSerial = mySerial5;
 
 void setup(){
   chumpSetup();
@@ -18,4 +13,3 @@ void setup(){
 void loop(){
   chumpLoop();
 }
-
