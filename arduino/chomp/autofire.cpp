f1a2fc90fd52a6f0511fc15e9c3c9dde3b904cc6
Jo Balme <jo.balme@gmail.com>
2016-04-04 23:20:40 -0700

    Adding predictive targeting option

--- arduino/chomp/autofire.cpp
+++ arduino/chomp/autofire.cpp
@@ -15,7 +15,6 @@
 #define FIRE_THRESHOLD 70
 #define CONTIG_THRESHOLD 2
 LeddarState getState(unsigned int detection_count, Detection* detections){
-
   int last_detected_segment = 0;
   int contiguous = 1;
   for (int i = 0; i < detection_count; i++){
@@ -48,6 +47,32 @@ LeddarState getState(unsigned int detection_count, Detection* detections){
   }
 
   if (contiguous >= CONTIG_THRESHOLD){
+    return ARM_ZONE;
+  }
+  return FAR_ZONE;
+}
+
+struct HitBox
+{
+  int16_t xmin;
+  int16_t xmax;
+  int16_t ymin;
+  int16_t ymax;
+  HitBox( int16_t _xmin, int16_t _xmax, int16_t _ymin, int16_t _ymax ) : xmin(_xmin), xmax(_xmax), ymin(_ymin), ymax(_ymax) { }
+};
+
+bool within(int16_t x, int16_t y, HitBox hb){
+  return (x <= hb.xmax && x >= hb.xmin && y <= hb.ymax && y >= hb.ymin); 
+}
+
+// Box-test predicted x/y for arm/fire regions
+LeddarState getStatePredictive(int16_t target_x_after_leadtime, int16_t target_y_after_leadtime){
+  const HitBox arm = { -50, 50, 60, 200 }; 
+  const HitBox hit = { -25, 25, 60, 100 };
+  if ( within(target_x_after_leadtime, target_y_after_leadtime, hit)){
+    return HIT_ZONE;
+  }
+  if ( within(target_x_after_leadtime, target_y_after_leadtime, arm)){
     return ARM_ZONE;
   }
   return FAR_ZONE;
