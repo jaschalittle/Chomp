9eb4fb5da08bd900688e5253a611a80e1b194c1b
Jo Balme <jo.balme@gmail.com>
2016-04-10 04:39:36 -0700

    Making leddar cosmos stuff real

--- arduino/chomp/telem.cpp
+++ arduino/chomp/telem.cpp
@@ -24,7 +24,7 @@ void send_sensor_telem(uint32_t loop_speed, int16_t pressure, uint16_t angle){
 }
 
 void sendLeddarTelem(Detection* detections, unsigned int count, LeddarState state){
-  const uint16_t packet_len = 39;
+  const uint16_t packet_len = 45;
   const uint8_t start = 0x02;
   const uint16_t ending = 0x6666;
   char sensor_data[packet_len] = {0};
@@ -33,11 +33,16 @@ void sendLeddarTelem(Detection* detections, unsigned int count, LeddarState stat
   offset += sizeof(uint8_t);
   memcpy(sensor_data + offset, &state, sizeof(int16_t));
   offset += sizeof(int16_t);
-  const uint16_t segments = 0x2301;
-  memcpy(sensor_data + offset, &segments, sizeof(uint16_t));
-  offset += sizeof(uint16_t);
+  const uint64_t segments = 0xEFCDAB8967452301;
+  memcpy(sensor_data + offset, &segments, sizeof(uint64_t));
+  offset += sizeof(uint64_t);
+  uint16_t j = 0;
   for (uint16_t i = 0; i < 16; i++){
-      uint16_t dist = random(i*10, i*10+5);
+      uint16_t dist = 1000;
+      if (detections[j].Segment == i){
+        dist = detections[j].Distance;
+        while (j < count && detections[j].Segment == i){j++;}
+      }
       memcpy(sensor_data + offset, &dist, sizeof(uint16_t));
       offset += sizeof(uint16_t);
   }
