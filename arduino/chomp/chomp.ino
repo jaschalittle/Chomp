1cd6d471c08c87626431564ccda3743dce15bc97
contradict <contradict@gmail.com>
2018-02-13 22:53:51 -0800

    These variables are defined here, not extern

--- arduino/chomp/chomp.ino
+++ arduino/chomp/chomp.ino
@@ -1,12 +1,12 @@
 #include "chomp_main.h"
 
-extern HardwareSerial& Debug = Serial;
-extern HardwareSerial& Xbee = Serial;           // RX pin 0, TX pin 1
-extern HardwareSerial& DriveSerial = Serial1;   // RX pin 19, TX pin 18
-extern HardwareSerial& LeddarSerial = Serial2;  // RX pin 17, TX pin 16
-extern HardwareSerial& Sbus = Serial3;          // RX pin 15, TX pin 14
+HardwareSerial& Debug = Serial;
+HardwareSerial& Xbee = Serial;           // RX pin 0, TX pin 1
+HardwareSerial& DriveSerial = Serial1;   // RX pin 19, TX pin 18
+HardwareSerial& LeddarSerial = Serial2;  // RX pin 17, TX pin 16
+HardwareSerial& Sbus = Serial3;          // RX pin 15, TX pin 14
 
-extern volatile bool g_enabled = false;
+volatile bool g_enabled = false;
 
 void setup(){
   chompSetup();
