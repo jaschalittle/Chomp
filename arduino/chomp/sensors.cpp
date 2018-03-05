1bf9e7b000619878850de87906cebf3545cda618
contradict <contradict@gmail.com>
2018-03-04 19:03:23 -0800

    Remove SPI IMU code

--- arduino/chomp/sensors.cpp
+++ arduino/chomp/sensors.cpp
@@ -3,29 +3,10 @@
 #include "pins.h"
 // #include "imu.h"
 #include "drive.h"
-#include <SPI.h>
-#include "MPU9250.h"
-
-#define SPI_CLOCK 8000000  // 8MHz clock works.
-
-#define SCK_PIN  52
-#define SS_PIN   A11 
-#define INT_PIN  3
-#define LED      13
-
-MPU9250 mpu(SPI_CLOCK, SS_PIN);
-
-uint32_t last_imu_read;
 
 void sensorSetup(){
     pinMode(ANGLE_AI, INPUT);
     pinMode(PRESSURE_AI, INPUT);
-    // SPI.begin();
-    // delay(2000);
-    // mpu.init(true, true);
-    // delay(2000);
-    // mpu.calib_acc();
-    // last_imu_read = micros();
 }
 
 // static const uint32_t pressure_sensor_range = 920 - 102;
@@ -118,76 +99,4 @@ bool angularVelocityBuffered (float* angular_velocity, const uint16_t* angle_dat
 
     *angular_velocity = (angle_max - angle_min) * 1000 / (DATAPOINTS_TO_AVERAGE * timestep_ms);
     return true;
-}
-
-float getYaccel() {
-    // reading * voltage (3.3v?) * ADC range = voltage, - zero voltage = signal voltage, / sensitivity = g
-    // return m/s/s
-    return mpu.accel_data[1] * 9.8 * 100;
-}
-
-float getZgyro() {
-    // reading * Vref / ADC range - Vzero / sensitivity = deg/s, / 360 * 2 * PI = rad/s?
-    return mpu.gyro_data[2] * DEG_TO_RAD;
-}
-
-#define IMU_BUFFER_SIZE 5
-#define Y_ACCEL_STOPPED_THRESHOLD 0.1  // figure out accel data at rest and set this properly
-#define DRIVE_STOPPED_THRESHOLD 10  // figure out what this should be. max drive command that doesn't move chomp.
-#define CHUMP_DRIVE_COEFF 0.09345191f  // cm/s per unit drive command on Chump
-float y_accel_readings[IMU_BUFFER_SIZE];
-float z_gyro_readings[IMU_BUFFER_SIZE];
-float imu_read_timesteps[IMU_BUFFER_SIZE];
-uint8_t imu_buffer_index = 0;
-uint32_t last_imu_time;
-void readImu(float* our_forward_vel, float* our_angular_vel) {
-    // get y accel reading, z gyro reading, timesteps, and add to ring buffers
-    mpu.read_all();
-    y_accel_readings[imu_buffer_index] = getYaccel();
-    z_gyro_readings[imu_buffer_index] = getZgyro();
-    imu_read_timesteps[imu_buffer_index] = (micros() - last_imu_time) / 1000;
-    last_imu_time = micros();
-    imu_buffer_index = (imu_buffer_index + 1) % IMU_BUFFER_SIZE;
-    float avg_y_accel = 0.0;
-    for (uint8_t i = 0; i < IMU_BUFFER_SIZE; i++) { avg_y_accel += y_accel_readings[i]; }
-    avg_y_accel /= IMU_BUFFER_SIZE;
-    
-    // if drive command below threshold (i.e. neutral) and y accel is below threshold, set velocity to 0 and zero out y accel buffer
-    // else, integrate over accel reading history to estimate velocity
-    if (avg_y_accel < Y_ACCEL_STOPPED_THRESHOLD && getAvgDriveCommand() < DRIVE_STOPPED_THRESHOLD) { 
-        *our_forward_vel = 0.0;
-        for (uint8_t i = 0; i < IMU_BUFFER_SIZE; i++) { y_accel_readings[i] = 0.0; }
-    } else {
-        float velocity_estimate = 0.0;
-        // iterate over ring buffer in two halves
-        for (uint8_t i = imu_buffer_index; i < IMU_BUFFER_SIZE; i++) {
-            velocity_estimate += y_accel_readings[i] * imu_read_timesteps[i] / 1000;
-        }
-        for (uint8_t i = 0; i < imu_buffer_index; i++) {
-            velocity_estimate += y_accel_readings[i] * imu_read_timesteps[i] / 1000;
-        }
-        *our_forward_vel = 0.9 * velocity_estimate + 0.1 * getAvgDriveCommand() * CHUMP_DRIVE_COEFF;
-    }
-    
-    // estimate angular velocity by averaging z gyro buffer
-    float angular_velocity_estimate = 0.0;
-    // iterate over ring buffer in two halves
-    for (uint8_t i = imu_buffer_index; i < IMU_BUFFER_SIZE; i++) {
-        angular_velocity_estimate += z_gyro_readings[i] * imu_read_timesteps[i];
-    }
-    for (uint8_t i = 0; i < imu_buffer_index; i++) {
-        angular_velocity_estimate += z_gyro_readings[i] * imu_read_timesteps[i];
-    }
-    angular_velocity_estimate /= IMU_BUFFER_SIZE;
-    *our_angular_vel = angular_velocity_estimate / 360 * 2 * PI;
-}
-
-// call this when tracking is started or restarted to store an initial IMU timestamp and zero out buffers
-// don't bother taking IMU readings, because they are useless without a timestep
-void resetImu() {
-    last_imu_time = micros();
-    for (uint8_t i = 0; i < IMU_BUFFER_SIZE; i++) {
-        y_accel_readings[i] = 0.0;
-        z_gyro_readings[i] = 0.0;
-    }
 }
